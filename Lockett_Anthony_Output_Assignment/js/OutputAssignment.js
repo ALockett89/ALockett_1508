4a84fa8991748eb7dafecf2b7019502618094505
ALockett89 <aklockett@fullsail.edu>
2015-08-05 21:47:38 -0500

    First draft
    Added variables I want to use.

---
+++ Lockett_Anthony_Output_Assignment/js/OutputAssignment.js
@@ -0,0 +1,17 @@
+/*
+ Anthony Lockett
+ SDI Week 1
+ Output Assignment
+ 8/5/15
+ */
+
+alert("This is in the js file");
+var myage = 26
+var yearofbirth = 1989
+var favteam = "New Orleans Saints"
+var superbowl = true
+
+console.log("My current age is" + " " + myage)
+console.log("The year I was born was" + " " + yearofbirth)
+console.log("My favorite NFL team is the" + ' ' + favteam + ".")
+console.log("Will the Saints win the Super Bowl? =" + " " + superbowl)
