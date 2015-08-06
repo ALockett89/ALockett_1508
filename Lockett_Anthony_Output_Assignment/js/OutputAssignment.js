ac88de74d74650e09c3bf0bf13c73a78a32c8efa
ALockett89 <aklockett@fullsail.edu>
2015-08-05 22:57:23 -0500

    3rd Round of Comments.
    Made some changes to the comments.

--- Lockett_Anthony_Output_Assignment/js/OutputAssignment.js
+++ Lockett_Anthony_Output_Assignment/js/OutputAssignment.js
@@ -7,12 +7,14 @@
 
 alert("Here is my Week 1 submission!");// Just a friendly alert that I added.
 
-var myage = 26// This is my number variable.
+var myage = 26// This is my first number variable.
 var yearofbirth = 1989// This is another number variable.
 var favteam = "New Orleans Saints"// This is my string variable.
-var superbowl = true// This is my boolean variable.
+var losesouth = false// This is my first boolean variable.
+var superbowl = true// This is my second boolean variable.
 
-console.log("My current age is" + " " + myage)// My console output showing the number variable.
+console.log("My current age is" + " " + myage)// My console output showing the first number variable.
 console.log("The year I was born was" + " " + yearofbirth)// My console output showing the second number variable.
 console.log("My favorite NFL team is the" + ' ' + favteam + ".")// My console output showing the string variable.
-console.log("Will the Saints win the Super Bowl? =" + " " + superbowl)// My console output showing the boolean variable.
+console.log("Will the Saints lose the NFC South division? This answer is" + " " + losesouth + " " + "my friend!")// My console output showing the first boolean variable.
+console.log("The Saints win the Super Bowl! This is a" + " " + superbowl + " " + "statement & every Saints' fan knows this!")// My console output showing the second boolean variable.
