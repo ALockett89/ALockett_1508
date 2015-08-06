1333be2e1b76cb5f41eafcaa13222f6a4e939b4d
ALockett89 <aklockett@fullsail.edu>
2015-08-05 21:53:13 -0500

    1st Round of comments
    I added my round of comments to my lines of code.

--- Lockett_Anthony_Output_Assignment/js/OutputAssignment.js
+++ Lockett_Anthony_Output_Assignment/js/OutputAssignment.js
@@ -5,12 +5,12 @@
  8/5/15
  */
 
-alert("Here is my Week 1 submission!");
+alert("Here is my Week 1 submission!");// Just a friendly alert that I added.
 
-var myage = 26
-var yearofbirth = 1989
-var favteam = "New Orleans Saints"
-var superbowl = true
+var myage = 26// This is my number variable.
+var yearofbirth = 1989// This is another number variable.
+var favteam = "New Orleans Saints"// This is my string variable.
+var superbowl = true// This is my boolean variable.
 
 console.log("My current age is" + " " + myage)
 console.log("The year I was born was" + " " + yearofbirth)
