67a9b2c3652f47da33f37806427d9c93aa1e2388
ALockett89 <aklockett@fullsail.edu>
2015-08-11 11:32:57 -0500

    2nd Draft
    Added more code & changed up some parts

--- Lockett Anthony Expressions Assignment/Expressions Assignment/js/ExpressionsAssignment.js
+++ Lockett Anthony Expressions Assignment/Expressions Assignment/js/ExpressionsAssignment.js
@@ -12,16 +12,24 @@ var weeks = [1,2]
 var hoursworked = prompt("How many hours did you work in a week?")
 console.log("You've worked" + space + hoursworked + space + "in a week!")
 var numofweeks = prompt("How many weeks in between pay checks? \nEnter A for one week. \nEnter B for two weeks")
-console.log("Weeks between pay checks:" + space + numofweeks)
+var A = weeks[0]
+var B = weeks[1]
+if (numofweeks = A){
 
-if (numofweeks = weeks[0]){
-    var hoursinaweek1 = hoursworked * numofweeks
-    console.log(hoursinaweek1)
+    console.log("Weeks between pay checks:" + space + A)
 }else{
-    (numofweeks = weeks[1])
-        var hoursinaweek2 = hoursworked *numofweeks
-    console.log(hoursinaweek2)
+    console.log("Weeks between pay checks:" + space + B)
+
+
+
+//if (numofweeks = weeks[0]){
+  //  var hoursinaweek1 = hoursworked * numofweeks
+    //console.log(hoursinaweek1)
+//}else{
+  //  (numofweeks = weeks[1])
+    //    var hoursinaweek2 = hoursworked *numofweeks
+    //console.log(hoursinaweek2)
 
 }
 
-console.log(numofweeks)
+//console.log(numofweeks)
