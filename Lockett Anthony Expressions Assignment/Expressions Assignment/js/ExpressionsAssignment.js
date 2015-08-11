9ab4e1840b940d60d074e73d454a35cf6c7b724f
ALockett89 <aklockett@fullsail.edu>
2015-08-10 21:32:10 -0500

    Added a new folder for week 2 Expressions Assignment

---
+++ Lockett Anthony Expressions Assignment/Expressions Assignment/js/ExpressionsAssignment.js
@@ -0,0 +1,9 @@
+/*
+ Anthony Lockett
+ SDI Week 2
+ Expressions Assignment
+ 8/10/15
+ */
+
+
+alert("Here is my Week 2 submission! \n Expressions Assignment");// Just a friendly alert that I added.
