71e2c7c2a60222afa53710c325cf64837ff38ddd
ALockett89 <aklockett@fullsail.edu>
2015-08-11 21:51:17 -0500

    3rd Draft
    Added all my code, now to work out the bugs.

--- Lockett Anthony Expressions Assignment/Expressions Assignment/js/ExpressionsAssignment.js
+++ Lockett Anthony Expressions Assignment/Expressions Assignment/js/ExpressionsAssignment.js
@@ -9,27 +9,31 @@
 alert("Here is my Week 2 submission! \n      Expressions Assignment");// Just a friendly alert that I added.
 var space = " "
 var weeks = [1,2]
-var hoursworked = prompt("How many hours did you work in a week?")
+var hoursworked = prompt("How many hours do you work in a week?")
 console.log("You've worked" + space + hoursworked + space + "in a week!")
-var numofweeks = prompt("How many weeks in between pay checks? \nEnter A for one week. \nEnter B for two weeks")
-var A = weeks[0]
-var B = weeks[1]
-if (numofweeks = A){
+var numofweeks = prompt("How many weeks in between pay checks? \nEnter '1' for one week. \nEnter '2' for two weeks")
+console.log("Weeks between your pay checks:" + space + numofweeks)
+var A = numofweeks * weeks[0]
+var B = numofweeks * weeks[1]
+var hours1 = A * hoursworked
+var hours2 = B * hoursworked
 
-    console.log("Weeks between pay checks:" + space + A)
+if(numofweeks <= weeks[2]){
+    console.log("Hours you worked this pay period:" + space + hours1)
 }else{
-    console.log("Weeks between pay checks:" + space + B)
-
-
-
-//if (numofweeks = weeks[0]){
-  //  var hoursinaweek1 = hoursworked * numofweeks
-    //console.log(hoursinaweek1)
-//}else{
-  //  (numofweeks = weeks[1])
-    //    var hoursinaweek2 = hoursworked *numofweeks
-    //console.log(hoursinaweek2)
-
+    console.log("Hours you worked this pay period:" + space + hours2)
 }
-
-//console.log(numofweeks)
+var payrate = prompt("Next, what is your hourly pay rate? \n *Please only use numbers*")
+console.log("Your wage is $" + payrate + space + "an hour!")
+alert("Now lets calculate your next paycheck! =} \nComputing................................")
+alert("................................Almost done \n................................")
+if(numofweeks <= weeks[2]){
+    var paycheck1 = payrate * hours1
+    alert("Your gross income on your next paycheck will be $:" + paycheck1 + "!" + "\nDon't spend it all in one place!")
+    console.log("Your gross income on your next paycheck will be $:" + paycheck1)
+}else{
+    var paycheck2 = payrate * hours2
+    alert("Your gross income on your next paycheck will be $:" + paycheck2 + "!" + "\nDon't spend it all in one place!")
+    console.log("Your gross income on your next paycheck will be $:" + paycheck2)
+}
+alert("Your gross income on your next paycheck will be $:" + paycheck1 + "!" + "\nDon't spend it all in one place!")
