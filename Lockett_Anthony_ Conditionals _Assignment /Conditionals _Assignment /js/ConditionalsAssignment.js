845d6abdf791039e506fbb715646aa14bd87fa52
ALockett89 <aklockett@fullsail.edu>
2015-08-19 21:06:15 -0500

    1st Draft
    Using some of the new things I’ve learned this week to make my Gross
    Income calculator better.

--- Lockett_Anthony_ Conditionals _Assignment /Conditionals _Assignment /js/ConditionalsAssignment.js
+++ Lockett_Anthony_ Conditionals _Assignment /Conditionals _Assignment /js/ConditionalsAssignment.js
@@ -1,1 +1,22 @@
-alert("This is in the js file");
+/*
+ Anthony Lockett
+ SDI Week 3
+ Expressions Assignment
+ 8/17/15
+ */
+
+
+alert("          Here is my Week 3 submission! \n               Conditionals Assignment");// Just a friendly alert that I added.
+var space = " "
+var weeks = [1,2]
+var weeksBetweenPay = true
+weeksBetweenPay = confirm("Do you get paid every week? \nPress OK for Yes \nPress CANCEL for No")
+console.log("Do you get paid every week:" + space + weeksBetweenPay)
+if(weeksBetweenPay = true){
+    var hoursworked = prompt("How many hours do you work in a week?")
+    console.log("You've worked" + space + hoursworked + space + "in a week!")
+}
+var taxes = true
+taxes = confirm("Do you pay taxes in Louisiana? \nPress OK for Yes \nPress CANCEL for No")
+console.log("Do you pay taxes in Louisiana:" + space + taxes)
+
