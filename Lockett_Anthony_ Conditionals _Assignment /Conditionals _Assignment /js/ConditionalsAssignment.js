72fc2f7428803fc5f31875fe8b3984a2d81512fa
ALockett89 <aklockett@fullsail.edu>
2015-08-20 17:33:11 -0500

    3 Draft
    Trying to get my code together.

--- Lockett_Anthony_ Conditionals _Assignment /Conditionals _Assignment /js/ConditionalsAssignment.js
+++ Lockett_Anthony_ Conditionals _Assignment /Conditionals _Assignment /js/ConditionalsAssignment.js
@@ -14,17 +14,33 @@ weeksBetweenPay = confirm("Do you get paid every week? \nPress OK for Yes \nPres
 console.log("Do you get paid every week:" + space + weeksBetweenPay)
 if(weeksBetweenPay === true) {
     var hoursworked = prompt("How many hours do you work in a week?")
-    while(hoursworked === "" || isNaN(hoursworked)) {
-        if(hoursworked === ""){
+    while (hoursworked === "" || isNaN(hoursworked)) {
+        if (hoursworked === "") {
             hoursworked = prompt("How many hours do you work in a week?\n**Please Do Not Leave Blank!**")
-        }else if(isNaN(hoursworked)){
+        } else if (isNaN(hoursworked)) {
             hoursworked = prompt("How many hours do you work in a week?\n** Please Only Use Nnumbers!**")
-        }else{
         }
-    }console.log("You've worked" + space + hoursworked + space + "hours in a week!")
+        console.log("You've worked" + space + hoursworked + space + "hours in a week!")
+    }
+    var dollarsbyhours = prompt("How much do you make an hour?")
+    while (dollarsbyhours === "" || isNaN(dollarsbyhours)) {
+        if (dollarsbyhours === "") {
+            dollarsbyhours = prompt("How much do you make an hour?\n**Please Do Not Leave Blank!**")
+        } else if (isNaN(dollarsbyhours)) {
+            dollarsbyhours = prompt("How much do you make an hour?\n** Please Only Use Nnumbers!**")
+        }
+        console.log("You've worked" + space + hoursworked + space + "hours in a week!")
+        var gross = weeks[1] * hoursworked
+        console.log("Your gross income is $" + gross + "!")
+
+    }
+}else{
 
 }
 var taxes = true
 taxes = confirm("Do you pay taxes in Louisiana? \nPress OK for Yes \nPress CANCEL for No")
+console.log("You've worked" + space + hoursworked + space + "hours in a week!")
+console.log("Your gross income is $" + gross + "!")
+console.log("You've worked" + space + hoursworked + space + "hours in a week!")
 console.log("Do you pay taxes in Louisiana:" + space + taxes);
 
