b99fa402e139cc212e935592eebb7efa55ed97c6
ALockett89 <aklockett@fullsail.edu>
2015-08-19 21:48:40 -0500

    2nd Draft
    Added more code.

--- Lockett_Anthony_ Conditionals _Assignment /Conditionals _Assignment /js/ConditionalsAssignment.js
+++ Lockett_Anthony_ Conditionals _Assignment /Conditionals _Assignment /js/ConditionalsAssignment.js
@@ -12,11 +12,19 @@ var weeks = [1,2]
 var weeksBetweenPay = true
 weeksBetweenPay = confirm("Do you get paid every week? \nPress OK for Yes \nPress CANCEL for No")
 console.log("Do you get paid every week:" + space + weeksBetweenPay)
-if(weeksBetweenPay = true){
+if(weeksBetweenPay === true) {
     var hoursworked = prompt("How many hours do you work in a week?")
-    console.log("You've worked" + space + hoursworked + space + "in a week!")
+    while(hoursworked === "" || isNaN(hoursworked)) {
+        if(hoursworked === ""){
+            hoursworked = prompt("How many hours do you work in a week?\n**Please Do Not Leave Blank!**")
+        }else if(isNaN(hoursworked)){
+            hoursworked = prompt("How many hours do you work in a week?\n** Please Only Use Nnumbers!**")
+        }else{
+        }
+    }console.log("You've worked" + space + hoursworked + space + "hours in a week!")
+
 }
 var taxes = true
 taxes = confirm("Do you pay taxes in Louisiana? \nPress OK for Yes \nPress CANCEL for No")
-console.log("Do you pay taxes in Louisiana:" + space + taxes)
+console.log("Do you pay taxes in Louisiana:" + space + taxes);
 
