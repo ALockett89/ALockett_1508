47df64a00be8a21da255a275dd1e023c0fcf9dbb
ALockett89 <aklockett@fullsail.edu>
2015-08-20 20:56:54 -0500

    5th Draft
    I was stuck with making the “hours worked2” prompt be a number but i
    figured it out!

--- Lockett_Anthony_ Conditionals _Assignment /Conditionals _Assignment /js/ConditionalsAssignment.js
+++ Lockett_Anthony_ Conditionals _Assignment /Conditionals _Assignment /js/ConditionalsAssignment.js
@@ -22,7 +22,17 @@ if(weeksBetweenPay === true) {
         }
     }console.log("You've worked" + space + hoursworked + space + "hours in a week!")
 } else {
-
+    if (weeksBetweenPay === false) {
+        var hoursworked2 = prompt("How many hours do you work in a week?")
+        while (hoursworked2 === "" || isNaN(hoursworked2)) {
+            if (hoursworked2 === "") {
+                hoursworked2 = prompt("How many hours do you work in a week?\n**Please Do Not Leave Blank!**")
+            } else if (isNaN(hoursworked2)) {
+                hoursworked2 = prompt("How many hours do you work in a week?\n** Please Only Use Nnumbers!**")
+            }
+        }var hoursworked3 = Number(hoursworked2)
+        console.log("You've worked" + space + hoursworked2 + space + "hours in a week!")
+    }
 }
 if(weeksBetweenPay === true) {
     var dollarsbyhours = prompt("How much do you make an hour?")
@@ -35,11 +45,33 @@ if(weeksBetweenPay === true) {
     } var gross1 = weeks[0] * hoursworked * dollarsbyhours
     console.log("Your gross income is $" + gross1 + "!")
 } else {
-
+    if (weeksBetweenPay === false) {
+        var dollarsbyhours2 = prompt("How much do you make an hour?")
+        while (dollarsbyhours2 === "" || isNaN(dollarsbyhours2)) {
+            if (dollarsbyhours2 === "") {
+                dollarsbyhours2 = prompt("How much do you make an hour?\n**Please Do Not Leave Blank!**")
+            } else if (isNaN(dollarsbyhours2)) {
+                dollarsbyhours2 = prompt("How much do you make an hour?\n** Please Only Use Nnumbers!**")
+            }
+        }
+        var gross2 = (hoursworked3 + hoursworked3) * dollarsbyhours2
+        console.log("Your gross income is $" + gross2 + "!")
+    }
 }
 var taxes = true
 taxes = confirm("Do you pay taxes in Louisiana? \nPress OK for Yes \nPress CANCEL for No")
-
+if(weeksBetweenPay === true) {
+    var dollarsbyhours = prompt("How much do you make an hour?")
+    while (dollarsbyhours === "" || isNaN(dollarsbyhours)) {
+        if (dollarsbyhours === "") {
+            dollarsbyhours = prompt("How much do you make an hour?\n**Please Do Not Leave Blank!**")
+        } else if (isNaN(dollarsbyhours)) {
+            dollarsbyhours = prompt("How much do you make an hour?\n** Please Only Use Nnumbers!**")
+        }
+    }
+    var gross1 = weeks[0] * hoursworked * dollarsbyhours
+    console.log("Your gross income is $" + gross1 + "!")
+}
 
 console.log("Do you pay taxes in Louisiana:" + space + taxes);
 
