4c8e98824e1330f25acd85ce75d59509197e3c16
ALockett89 <aklockett@fullsail.edu>
2015-08-24 21:19:47 -0500

    Setting up Week 4
    Making a “Functions Assignment” directory.

---
+++ Lockett_Anthony_Functions _Assignment /Functions _Assignment /js/FunctionsAssignment.js
@@ -0,0 +1,9 @@
+/*
+ Anthony Lockett
+ SDI Week 4
+ Functions Assignment
+ 8/24/15
+ */
+
+
+alert("          Here is my Week 4 submission! \n               Functions Assignment");// Just a friendly alert that I added.
