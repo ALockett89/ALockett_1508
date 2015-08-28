a1419a045eba3325c5835c5fb59b64517fb1a65a
ALockett89 <aklockett@fullsail.edu>
2015-08-27 23:21:05 -0500

    4th Draft 2.0
    moved code to the correct directory

--- Lockett_Anthony_Functions _Assignment /Functions _Assignment /js/FunctionsAssignment.js
+++ Lockett_Anthony_Functions _Assignment /Functions _Assignment /js/FunctionsAssignment.js
@@ -8,54 +8,79 @@
 
 alert("          Here is my Week 4 submission! \n               Functions Assignment");// Just a friendly alert that I added.
 
-var space = " "
 var age = 21
-var ageprompt = prompt("Please Enter Your Age!")
-var flolotto = "Florida Lottery"
-var powlotto = "Powerball Lottery"
-var timesskipped = 1
+var space = " "
 
-function agecheck() {
-    if (ageprompt < age) {
+function ageplease(){
+    var ageprompt = prompt("Please Enter Your Age!")
+    while(ageprompt==="" || isNaN(ageprompt)){
+        if(ageprompt===""){
+            ageprompt= prompt("**PLEASE DO NOT LEAVE BLANK**\nPlease Enter Your Age!")
+        }else if(isNaN(ageprompt)){
+            ageprompt = prompt("**ONLY USE NUMBERS**\nPlease Enter Your Age!")
+        }
+    }return ageprompt
+}
+var findage = ageplease()
+console.log("User's age is" + space + findage + "!")
+
+function canyouplay() {
+    if (findage < age) {
+        var ageprompt2 = false
         alert("Sorry, come back when you are older")
         console.log("User is under the age of" + space + age + "!")
     } else {
+        var ageprompt2 = true
         alert("Welcome!\nNow lets pick a lottery!")
         console.log("User is over the age of" + space + age + "!")
-    }
+    }return ageprompt2
 }
-
+var findage2 = canyouplay()
+console.log("Is the user old enough to play:" + space + findage2)
 
 function whichlotto(){
-    var flolotto2 = 1
-    var powlotto2 = 2
-    var whichlotto2 = prompt("Which lottery are you playing?\nEnter '1' for Florida Lottery.\nEnter '2' for Powerball Lottery")
-    if (whichlotto2 = 1){
-        alert("Ok, so" + space + flolotto + space + "it is then!")
-        console.log("User picked" + flolotto2 + "!")
+    if (findage2 === true){
+        var lottoprompt = prompt("Which lottery are you playing?\nEnter '1' for Florida Lottery.\nEnter '2' for Powerball Lottery")
+        while(lottoprompt==="" || isNaN(lottoprompt)){
+            if(lottoprompt===""){
+                lottoprompt= prompt("**PLEASE DO NOT LEAVE BLANK**\nWhich lottery are you playing?\nEnter '1' for Florida Lottery.\nEnter '2' for Powerball Lottery")
+            }else if(isNaN(lottoprompt)){
+                lottoprompt = prompt("**ONLY USE NUMBERS**\nWhich lottery are you playing?\nEnter '1' for Florida Lottery.\nEnter '2' for Powerball Lottery")
+            }
+        }
+        if (lottoprompt < 2){
+            var lottoprompt2 = 1
+            alert("OK, you have chosen to play the Florida Lottery!\nLet's Begin!")
+            console.log("User has chosen to play the Florida Lottery!")
+        }else{
+            var lottoprompt2 = 2
+            alert("OK, you have chosen to play the Powerball Lottery!\nLet's Begin!")
+            console.log("User has chosen to play the Powerball Lottery!")
+        }
+    }return lottoprompt2
+}
+var lottochosen = whichlotto()
+
+function truefalse(){
+    if (lottochosen === 1){
+        var truefalse = true
     }else{
-        alert("Ok, so" + space + powlotto + space + "it is then!")
-        console.log("User picked" + powlotto2 + "!")
-    }
+        var truefalse = false
+    }return truefalse
+}console.log(truefalse())
+var mathtime = truefalse()
 
+if (mathtime === false){
+    function flolotto(){
+        var lottotrue = [];
+        for (var i = 0; i < 6; i++) {
+            lottotrue[i] = Math.random() * (53 - 1) + 1
+            lottotrue[i] = Math.round(lottotrue[i])
+        }
+        return flolotto();
+    }
 }
 
-function lottopick(){
-    if (agecheck() === false){
-        whichlotto()
 
-    }
-}
-
-agecheck()
-lottopick()
-
-
-
-
-
-
-
-
-
-
+var usergame = flolotto();
+console.log(usergame)
