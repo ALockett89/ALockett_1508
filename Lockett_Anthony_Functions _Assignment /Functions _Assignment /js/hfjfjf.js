60e34e3217db275a41fb2abc4c25713d9f3c25ae
ALockett89 <aklockett@fullsail.edu>
2015-08-27 21:26:54 -0500

    3rd draft
    I finally got a grasp on functions & scrapped old code for this new
    version that WORKS!!!!!!

--- Lockett_Anthony_Functions _Assignment /Functions _Assignment /js/hfjfjf.js
+++ Lockett_Anthony_Functions _Assignment /Functions _Assignment /js/hfjfjf.js
@@ -1,74 +1,62 @@
-/**
- * Created by TokyoGNSD1 on 8/26/15.
+/*
+ Anthony Lockett
+ SDI Week 4
+ Functions Assignment
+ 8/24/15
  */
 
 
+alert("          Here is my Week 4 submission! \n               Functions Assignment");// Just a friendly alert that I added.
 
-var space = " "
 var age = 21
-var ageprompt = prompt("Please Enter Your Age!")
+var space = " "
 
-function agecheck() {
-    while (ageprompt < age){
-        alert("Sorry")
-    }
-
-}
-
-agecheck()
-
-
-
-
-
-//if(weeksBetweenPay === true) {
-  //  var hoursworked = prompt("How many hours do you work in a week?")// This variable is a prompt that is asking for the number of hours the user has worked in a week.
-    //while (hoursworked === "" || isNaN(hoursworked)) {
-      //  if (hoursworked === "") {
-        //    hoursworked = prompt("How many hours do you work in a week?\n**Please Do Not Leave Blank!**")// This variable is a prompt that is asking for the number of hours the user has worked in a week.
-        //} else if (isNaN(hoursworked)) {
-         //   hoursworked = prompt("How many hours do you work in a week?\n** Please Only Use Nnumbers!**")// This variable is a prompt that is asking for the number of hours the user has worked in a week.
-        //}
-    //}console.log("You've worked" + space + hoursworked + space + "hours in a week!")// This outputs the user's input to the console.
-//} else {
-  //  if (weeksBetweenPay === false) {
-    //    var hoursworked2 = prompt("How many hours do you work in a week?")// This variable is a prompt that is asking for the number of hours the user has worked in a week.
-      //  while (hoursworked2 === "" || isNaN(hoursworked2)) {
-        //    if (hoursworked2 === "") {
-          //      hoursworked2 = prompt("How many hours do you work in a week?\n**Please Do Not Leave Blank!**")// This variable is a prompt that is asking for the number of hours the user has worked in a week.
-           // } else if (isNaN(hoursworked2)) {
-            //    hoursworked2 = prompt("How many hours do you work in a week?\n** Please Only Use Nnumbers!**")// This variable is a prompt that is asking for the number of hours the user has worked in a week.
-           // }
-        //}var hoursworked3 = Number(hoursworked2)
-        //console.log("You've worked" + space + hoursworked2 + space + "hours in a week!")// This outputs the user's input to the console.
-    //}
-//}
-
-
-alert("Welcome!\nNow lets pick a lottery!")
-console.log("User is over the age of" + space + age + "!")
-
-var lottoprompt = prompt("Which lottery are you playing?\nFlorida Lottery\nor\nPowerball Lottery")
-function lotto(whichlotto) {
-    while (whichlotto != flolotto) {
-        var whichlotto2 = prompt("**Enter A Correct Answer*\n**Case Sensitive**\nWhich lottery are you playing?\nFlorida Lottery\nor\nPowerball Lottery")
-        timesskipped++
-        if (timesskipped === 4) {
-            alert("Just come back when you are ready! =]")
-            console.log("User did not enter a correct response in the allowed amount of attempts")
-            break
-
-        }else{
-            alert("Awesome! Lets play the" + space + flolotto + "!")
-            console.log("User choose" + space + flolotto + "!")
-
+function ageplease(){
+    var ageprompt = prompt("Please Enter Your Age!")
+    while(ageprompt==="" || isNaN(ageprompt)){
+        if(ageprompt===""){
+            ageprompt= prompt("**PLEASE DO NOT LEAVE BLANK**\nPlease Enter Your Age!")
+        }else if(isNaN(ageprompt)){
+            ageprompt = prompt("**ONLY USE NUMBERS**\nPlease Enter Your Age!")
         }
-
-
-    }
-
-
-
-}lotto(lottoprompt)
-
+    }return ageprompt
 }
+var findage = ageplease()
+console.log("User's age is" + space + findage + "!")
+
+function canyouplay() {
+    if (findage < age) {
+        var ageprompt2 = false
+        alert("Sorry, come back when you are older")
+        console.log("User is under the age of" + space + age + "!")
+    } else {
+        var ageprompt2 = true
+        alert("Welcome!\nNow lets pick a lottery!")
+        console.log("User is over the age of" + space + age + "!")
+    }return ageprompt2
+}
+var findage2 = canyouplay()
+console.log("Is the user old enough to play:" + space + findage2)
+
+function whichlotto(){
+    if (findage2 === true){
+        var lottoprompt = prompt("Which lottery are you playing?\nEnter '1' for Florida Lottery.\nEnter '2' for Powerball Lottery")
+        while(lottoprompt==="" || isNaN(lottoprompt)){
+            if(lottoprompt===""){
+                lottoprompt= prompt("**PLEASE DO NOT LEAVE BLANK**\nWhich lottery are you playing?\nEnter '1' for Florida Lottery.\nEnter '2' for Powerball Lottery")
+            }else if(isNaN(lottoprompt)){
+                lottoprompt = prompt("**ONLY USE NUMBERS**\nWhich lottery are you playing?\nEnter '1' for Florida Lottery.\nEnter '2' for Powerball Lottery")
+            }
+        }
+        if (lottoprompt < 2){
+            var lottoprompt2 = true
+            alert("OK, you have chosen to play the Florida Lottery!\nLet's Begin!")
+            console.log("User has chosen to play the Florida Lottery!")
+        }else{
+            var lottoprompt2 = false
+            alert("OK, you have chosen to play the Powerball Lottery!\nLet's Begin!")
+            console.log("User has chosen to play the Powerball Lottery!")
+        }return lottoprompt2
+    }
+}
+var lottochosen = whichlotto()
